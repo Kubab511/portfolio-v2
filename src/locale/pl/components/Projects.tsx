dde30e1ebc419d35dea53b2976f890e10091ba07
Kubab511 <111919220+Kubab511@users.noreply.github.com>
2024-09-05 23:25:15 +0200

    Adjusted links in project cards

--- src/locale/pl/components/Projects.tsx
+++ src/locale/pl/components/Projects.tsx
@@ -32,12 +32,14 @@ export function Projects() {
                 <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
                   <NavLink
                     to={project.gitUrl}
+                    target="_blank"
                     className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/NavLink"
                   >
                     <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/NavLink:text-white" />
                   </NavLink>
                   <NavLink
                     to={project.previewUrl}
+                    target="_blank"
                     className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/NavLink"
                   >
                     <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/NavLink:text-white" />
