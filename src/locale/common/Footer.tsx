fec41faa729205254e9ffb61fe9711deef382db8
Kubab511 <barabasz.kuba@gmail.com>
2024-11-15 16:59:55 +0000

    Refactor code

--- src/locale/common/Footer.tsx
+++ src/locale/common/Footer.tsx
@@ -2,7 +2,6 @@ export function Footer() {
   return (
     <div className="footer border border-transparent border-t-[#484b6a] text-slate-950">
       <div className="container mx-auto p-6 flex flex-col items-center justify-end min-h-fit sm:flex-row">
-        {/* <img className="h-10 w-10" src="/favicon.webp" alt="Logo" /> */}
         <p className="text-slate-600 sm:pt-0 pt-2 my-auto pl-4">&#169; 2024 Jakub Barabasz</p>
       </div>
     </div>
--- src/locale/pl/AppPl.tsx
+++ src/locale/pl/AppPl.tsx
@@ -1,5 +1,5 @@
 import { About } from "./components/About"
-import { Footer } from "./components/Footer"
+import { Footer } from "../common/Footer"
 import { Hero } from "./components/Hero"
 import { Navbar } from "./components/Navbar"
 import { Projects } from "./components/Projects"
